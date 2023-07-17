70b80367fe955189643b6f05348f8372da77498c
kiinosuke13 <kii613.penguin@gmail.com>
2023-07-18 08:27:53 +0900

    ファイルを変更

--- main.js
+++ main.js
@@ -1,18 +1,14 @@
-'use strict';
-
-const inputText = document.getElementById('input_text');
-function keyUp(){
-    let str = inputText.value;
-    let num = 10 - str.length;
-    console.log(num);
-    const count = document.getElementById('count');
-    count.textContent = num;
-    const countWrap = document.getElementById('countWrap');
-    if(num > 0) {
-        countWrap.style.color = "black";
-    } else {
-        countWrap.style.color = "red"; 
-    }
+const inputText = document.getElementById("input_text");
+function keyUp() {
+  let str = inputText.value;
+  const count = document.getElementById("count");
+  let num = 10 - str.length;
+  count.textContent = num;
+  if (num >= 0) {
+    count.style.color = "black";
+  } else {
+    count.style.color = "red";
+  }
 }
 
-inputText.addEventListener('keyup',keyUp,false);
+inputText.addEventListener("keyup", keyUp, false);
